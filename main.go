05c1aefb9297f095eea534bd591954ab5bc3bace
apexcoder007 <saurabhdubey96440@gmail.com>
2025-05-16 05:33:50 +0530

    added functionality to checkout books

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -23,6 +24,44 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func bookByID(c *gin.Context) {
+	id := c.Param("id")
+	book, err := getBookByID(id)
+	if err != nil {
+		return
+	}
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func checkoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "missing ID query parameter"})
+		return
+	}
+	book, err := getBookByID(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found."})
+		return
+	}
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "book not available."})
+		return
+	}
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+
+}
+
+func getBookByID(id string) (*book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("book not found")
+}
+
 func createBook(c *gin.Context) {
 	var newBook book
 	if err := c.BindJSON(&newBook); err != nil {
@@ -35,6 +74,8 @@ func createBook(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.GET("/books/:id", bookByID)
 	router.POST("/books", createBook)
+	router.PATCH("/checkout", checkoutBook)
 	router.Run("localhost:8080")
 }
